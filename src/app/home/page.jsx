91a1f3d30d92a0f880b2cd618b60612149de2176
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-08-12 11:21:22 -0300

    Create Line Component

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,21 +1,27 @@
 import styles from "./Home.module.css";
 import Header from "../../components/Header";
 import Button from "../../components/Button";
+import Line from "../../components/Line";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Header />
-      <div className={styles.text}>
-        <h1 className={styles.title}>Bem-vindo ao MiniWiki Tech</h1>
-        <p className={styles.description}>Explore uma variedade de tópicos relacionados à tecnologia e amplie seu conhecimento.</p>
-        <p className={styles.description}>Fique à vontade para navegar e aprender mais sobre cada assunto!</p>
-      </div>
+      <div className={styles.card}>
+        <div className={styles.text}>
+          <h1 className={styles.title}>Bem-vindo ao MiniWiki Tech</h1>
 
-      <Button text="PÁGINA 1" href="/pag1" />
-      <Button text="PÁGINA 2" href="/pag2" />
-      <Button text="PÁGINA 3" href="/pag3" />
-      <Button text="PÁGINA 4" href="/pag4" />
+          <p className={styles.description}>Explore uma variedade de tópicos relacionados à tecnologia e amplie seu conhecimento.</p>
+          <p className={styles.description}>Fique à vontade para navegar e aprender mais sobre cada assunto!</p>
+        </div>
+        
+        <Line />
+
+        <Button text="PÁGINA 1" href="/pag1" />
+        <Button text="PÁGINA 2" href="/pag2" />
+        <Button text="PÁGINA 3" href="/pag3" />
+        <Button text="PÁGINA 4" href="/pag4" />
+      </div>
     </div>
   );
 }
---
+++ src/components/Line.jsx
@@ -0,0 +1,7 @@
+import styles from "../styles/Line.module.css"
+
+export default function Line() {
+    return (
+        <div className={styles.line}></div>
+    )
+}
