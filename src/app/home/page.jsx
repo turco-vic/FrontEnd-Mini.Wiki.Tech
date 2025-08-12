a4558125e8af499299bd11d7b5357aea56391c7e
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-08-12 10:54:27 -0300

    Create Button Component

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,31 +1,21 @@
 import styles from "./Home.module.css";
 import Header from "../../components/Header";
-import Link from "next/link";
+import Button from "../../components/Button";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Header />
-      <h1 className={styles.title}>Bem-vindo ao MiniWiki Tech</h1>
-      <p className={styles.description}>Explore uma variedade de tópicos relacionados à tecnologia e amplie seu conhecimento.</p>
-      <p className={styles.description}>Fique à vontade para navegar e aprender mais sobre cada assunto!</p>
+      <div className={styles.text}>
+        <h1 className={styles.title}>Bem-vindo ao MiniWiki Tech</h1>
+        <p className={styles.description}>Explore uma variedade de tópicos relacionados à tecnologia e amplie seu conhecimento.</p>
+        <p className={styles.description}>Fique à vontade para navegar e aprender mais sobre cada assunto!</p>
+      </div>
 
-      <Link href="/page1">
-        <button className={styles.navButton}>Página 1</button>
-      </Link>
-
-      <Link href="/page2">
-        <button className={styles.navButton}>Página 2</button>
-      </Link>
-
-      <Link href="/page3">
-        <button className={styles.navButton}>Página 3</button>
-      </Link>
-
-      <Link href="/page4">
-        <button className={styles.navButton}>Página 4</button>
-      </Link>
-
+      <Button text="PÁGINA 1" href="/pag1" />
+      <Button text="PÁGINA 2" href="/pag2" />
+      <Button text="PÁGINA 3" href="/pag3" />
+      <Button text="PÁGINA 4" href="/pag4" />
     </div>
   );
 }
